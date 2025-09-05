220767fd3898334477f72af38f87e74d4ca7b654
Arcemontoya <a127124@uabc.edu.mx>
2025-09-05 11:36:19 -0700

    React + JoyUI frontend

--- frontend/src/templates/SendMessage.jsx
+++ frontend/src/templates/SendMessage.jsx
@@ -26,7 +26,7 @@ function SendMessage() {
 
       // Llamamos a la Lambda vía API Gateway
       await axios.post(
-        "https://fo5m44quzd.execute-api.us-east-1.amazonaws.com/Prod/send",
+        VITE_API_BASE, // Variable ENV
         payload,
         { headers: { "Content-Type": "application/json" } }
       );
