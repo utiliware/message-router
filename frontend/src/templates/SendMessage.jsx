77fde6b6ee977675173c72b7bb50ffb9c92ff8d7
Stellios <stellios212003@gmail.com>
2025-09-06 15:39:26 -0700

    xray

--- frontend/src/templates/SendMessage.jsx
+++ frontend/src/templates/SendMessage.jsx
@@ -26,7 +26,7 @@ function SendMessage() {
 
       // Llamamos a la Lambda vía API Gateway
       await axios.post(
-        VITE_API_BASE, // Variable ENV
+        "https://d1wythkkdg.execute-api.us-east-1.amazonaws.com/Prod/send", // Variable ENV
         payload,
         { headers: { "Content-Type": "application/json" } }
       );
